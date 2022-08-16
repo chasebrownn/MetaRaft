f1bf1f4374a23aabcf56ddbca8d188ab6e49a4f7
Matthew Merrill <4mmerrill@gmail.com>
2022-08-16 10:21:55 -0700

    Added Support Files

--- src/Art.sol
+++ src/Art.sol
@@ -4,6 +4,23 @@ pragma solidity ^0.8.13;
 import "src/openzeppelin-contracts-master/contracts/access/Ownable.sol";
 
 
-contract Art is Ownable {
+contract Art is Ownable, ERC721 {
     
+uint tokenId;
+mapping(address=>tokenMetaData[]) public ownershipRecord;
+
+struct tokenMetaData{
+    uint tokenId;
+    uint timeStamp;
+    string tokenURI;
+}
+
+function mintToken(address recipient) onlyOwner public {
+    require(owner()!=recipient, "Recipient cannot be the owner of the contract");
+    _safeMint(recipient, tokenId);
+    ownershipRecord[recipient].push(tokenMetaData(tokenId, block.timestamp, "https://miro.medium.com/max/1120/1*k_EY7dcLYB5Z5k8zhMcv6g.png"));
+    tokenId = tokenId + 1;
+}
+
+
 }
