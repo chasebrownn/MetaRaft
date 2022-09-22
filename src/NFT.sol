0e1ea772cf4eeaa18b5102859a7a3b5026e23daa
Matthew Merrill <4mmerrill@gmail.com>
2022-09-22 08:57:33 -0700

    Fixed Forge

--- src/NFT.sol
+++ src/NFT.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import "../lib/openzeppelin-contracts/contracts/access/Ownable.sol";
-import "../lib/openzeppelin-contracts/contracts/utils/Strings.sol";
-import "./libraries/ERC721.sol";
+import "src/libraries/Ownable.sol";
+import "src/libraries/Strings.sol";
+import "src/interfaces/ERC721.sol";
 
 contract NFT is ERC721, Ownable {
     using Strings for uint256;
--- src/Rewards.sol
+++ src/Rewards.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import "../lib/openzeppelin-contracts/contracts/access/Ownable.sol";
+import "src/libraries/Ownable.sol";
 
 contract Rewards is Ownable {
     // ---------------
