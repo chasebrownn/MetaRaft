137ddac3ce610626847201325c4bb8f62372fcbf
jlw261 <jlw261@miami.edu>
2022-10-18 16:49:14 -0400

    Update state variables for raffle, fix onlyowner permission errors

--- src/Rewards.sol
+++ src/Rewards.sol
@@ -18,16 +18,26 @@ contract Rewards is Ownable {
     // State Variables
     // ---------------
 
+    // Contract Interactions
     address public stableCurrency;          /// @notice Used to store address of coin used to deposit/payout from Rewards.sol.
     address public nftContract;             /// @notice Used to store the address of the NFT contract.
     address public pythonScript;            /// @notice Used to store the address of the python script.
-    bool public redemptionEnabled;          /// @notice Used to enable/disable redemptions.
+   
+    // Rewards
+    bool public redemptionEnabled;              /// @notice Used to enable/disable redemptions.
+    uint public redeemWindowOpen;               /// @notice Used to store the block time of the redeem window open.
+    uint public constant redeemPeriod = 1;      /// @notice Used to store the minimum redemption window for NFT rewards.
+    mapping(uint => rewardsData) redeemState;   /// @notice Used to track NFT reward state data.
 
     enum rewardTiers {                       
         TIER_ONE, TIER_TWO, TIER_THREE, TIER_FOUR, TIER_FIVE, TIER_SIX
     }                                        /// @notice Used to store the rewards tier in an easier to read format.
 
-
+    struct rewardsData{
+        bool isWinner;
+        bool isRedeemed;
+        rewardTiers tier;
+    }
 
     // -----------
     // Constructor
@@ -60,7 +70,17 @@ contract Rewards is Ownable {
         _;
     }
 
+    // ------
+    // Events
+    // ------
 
+    /// @dev Emitted when the redemption window is opened via openRedeemWindow()
+    event RedemptionOpened(address indexed nftContract, uint redeemWindowOpen, uint minimumRedeemPeriod);
+
+    /// @dev Emitted when the redemption window is opened via closeRedeemWindow()
+    event RedemptionClosed(address indexed nftContract, uint redeemWindowOpen, uint realRedeemPeriod, uint redeemWindowClose);
+
+    event RewardsSet(address indexed nftContract, string randomNumberSource, uint randomNumberVerificationHash);
 
     // ---------
     // Functions
@@ -69,26 +89,34 @@ contract Rewards is Ownable {
     /// @notice Allows NFT contract to deposit mint USDC.
     /// @notice Allows user to invest ETH into the REIT.
     /// @dev ETH is not ERC20, needs to be wrapped using the WETH contract.
-    function depositETH() external payable isNFTContract() {
+    function depositETH() external payable isNFTContract {
 
     }
 
     /// @notice Allows owner to enable redeeming of rewards by users.
     /// @param _sanity Uint256 to verify sanity.
-    function openRedeemWindow(uint256 _sanity) external onlyOwner() {
+    /// TODO Add a call to NFT contract to verify there is no active sale
+    function openRedeemWindow(uint256 _sanity) external onlyOwner {
         require(_sanity == 42, "Rewards.sol::openRedeemWindow() _sanity must be 42 to confirm");
         require(redemptionEnabled == false, "Rewards.sol::openRedeemWindow() redemption window is already active");
 
         redemptionEnabled = true;
+        redeemWindowOpened = block.timestamp;
+
+        emit  RedemptionOpened(nftContract, redeemWindowOpened, redeemPeriod);
+
     }
 
     /// @notice Allows owner to disable redeeming of rewards by users.
     /// @param _sanity Uint256 to verify sanity.
-    function closeRedeemWindow(uint256 _sanity) external onlyOwner() {
+    function closeRedeemWindow(uint256 _sanity) external onlyOwner {
         require(_sanity == 42, "Rewards.sol::closeRedeemWindow() _sanity must be 42 to confirm");
         require(redemptionEnabled == true, "Rewards.sol::closeRedeemWindow() redemption window is already inactive");
-        
+        require((block.timestamp - redeemWindowOpened) >= redeemPeriod,"Rewards.sol::closeRedeemWindow() redemption window cannot before redemption period is over");
+
         redemptionEnabled = false;
+
+        emit RedemptionClosed(nftContract, redeemWindowOpened, (redeemWindowOpened-block.timestamp), block.timestamp);
 
     }   
 
@@ -98,25 +126,27 @@ contract Rewards is Ownable {
     }
 
     /// @notice Used to retrieve all winning IDs.
-    function getWinners() external onlyOwner() {
+    function getWinners() external onlyOwner {
 
     }
 
     /// @notice Used to determine if an NFT is above tier one. 
     /// @param _id NFT id that is atempting to be redeemed.
-    function getResults(uint256 _id) public onlyOwner() {
-
+    /// TODO Should not be onlyOwner, all users of the dapp must call this for the dapp to load properly
+    function getResults(uint256 _id) public onlyOwner returns(bool){
+        return(rewardsData[_id].isWinner);
     }
 
     /// @notice Used to determine if an NFT has already been redeemed. 
     /// @param _id NFT id that is atempting to be redeemed.
-    function isRedeemed(uint256 _id) public onlyOwner() {
-
+    /// TODO Should not be onlyOwner, all users of the dapp must call this for the dapp to load properly
+    function isRedeemed(uint256 _id) public onlyOwner returns(bool){
+        return(rewardsData[_id].isRedeemed);
     }
 
     /// @notice Used to set the python script address.
     /// @param _pythonScript The address the python script is using.
-    function setPythonScript(address _pythonScript) public onlyOwner() {
+    function setPythonScript(address _pythonScript) public onlyOwner {
         require(_pythonScript != pythonScript, "Rewards.sol pythonScript is already set to this address");
         pythonScript = _pythonScript;
 
